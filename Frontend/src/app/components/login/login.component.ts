f0216ece4120ae2eaa7ab7c3b0c0de782fb7a24d
lucy2512 <mail4parthib@gmail.com>
2023-01-29 21:03:02 +0530

    Login form validation done

--- Frontend/src/app/components/login/login.component.ts
+++ Frontend/src/app/components/login/login.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  loginForm!: FormGroup;
+  submitted = false;
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: ['',Validators.required],
+      password: ['',Validators.required]
+    });
+  }
+
+  onSubmit(){ 
+    if(this.loginForm.valid){
+      console.log(this.loginForm.value);
+    }
+    else{
+      console.log("Invalid LoginForm");
+      this.validateAllFormFields(this.loginForm);
+      alert("Invalid LoginForm")
+    }
+  }
+
+
+  private validateAllFormFields(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach(field => {
+      const control = formGroup.get(field);
+      if (control instanceof FormControl) {
+        control.markAsDirty({ onlySelf: true });
+      } else if (control instanceof FormGroup) {
+        this.validateAllFormFields(control);
+      }
+    });
+  }
+
+  Empty(){
+    this.submitted = true;
+    if(this.loginForm.invalid) return;
+  }
 }
+
+
