63f8003cd7f1b263f29844c155430d78b4cdac32
lucy2512 <mail4parthib@gmail.com>
2023-01-29 21:43:31 +0530

    DB Created

---
+++ Backend/Backend/Context/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Context
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("users");
+        }
+    }
+}
