f6472749c06d3199064b31bd75bf014e41c44ce2
lucy2512 <mail4parthib@gmail.com>
2023-01-31 08:48:15 +0530

    Backend Auth Done

---
+++ Backend/Backend/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using Backend.Context;
+using Backend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        public UserController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] User uobj)
+        {
+            if(uobj == null)
+            {
+                return BadRequest();
+            }
+
+            var user = await _applicationDbContext.Users.FirstOrDefaultAsync(x => x.Username == uobj.Username && x.Password == uobj.Password);
+            if(user == null)
+            {
+                return NotFound("User not found!");
+            }
+            return Ok("Login Successful!");
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register([FromBody] User uobj)
+        {
+            if(uobj == null)
+            {
+                return BadRequest();
+            }
+
+            await _applicationDbContext.Users.AddAsync(uobj);
+            await _applicationDbContext.SaveChangesAsync();
+            return Ok("Registration Successful!");
+
+        }
+    }
+}
