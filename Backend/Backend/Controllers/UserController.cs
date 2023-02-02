9dcd8b3561b736c134fc0db3bd01144f35246592
lucy2512 <mail4parthib@gmail.com>
2023-02-02 11:50:18 +0530

    Serverside Validation & Password Encryption Done

--- Backend/Backend/Controllers/UserController.cs
+++ Backend/Backend/Controllers/UserController.cs
@@ -1,10 +1,13 @@
 ﻿using Backend.Context;
 using Backend.Models;
+using Backend.Services;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Backend.Controllers
 {
@@ -26,7 +29,7 @@ namespace Backend.Controllers
                 return BadRequest();
             }
 
-            var user = await _applicationDbContext.Users.FirstOrDefaultAsync(x => x.Username == uobj.Username && x.Password == uobj.Password);
+            var user = await _applicationDbContext.Users.FirstOrDefaultAsync(x => x.Username == uobj.Username);
             if(user == null)
             {
                  return Unauthorized(new
@@ -34,6 +37,13 @@ namespace Backend.Controllers
                      Message = "User not found!"
                  });
                 //return StatusCode(StatusCodes.Status401Unauthorized, "User not found");
+            }
+            if(!(PasswordHasher.VerifyPassword(uobj.Password, user.Password)))
+            {
+                return BadRequest(new
+                {
+                    Message = "Password is incorrect"
+                });
             }
              return Ok(new
              {
@@ -50,6 +60,34 @@ namespace Backend.Controllers
                 return BadRequest();
             }
 
+            if(await CheckUserNameExist(uobj.Username))
+            {
+                return BadRequest(new
+                {
+                    Message = "Username already exist!"
+                });
+            }
+
+            if(await CheckEmailExist(uobj.Email))
+            {
+                return BadRequest(new
+                {
+                    Message = "Email is already in use"
+                });
+            }
+            var pw = CheckPasswordStrength(uobj.Password);
+            if (!string.IsNullOrEmpty(pw))
+            {
+                return BadRequest(new
+                {
+                    Message = pw.ToString()
+                });
+            }
+
+
+            uobj.Password = PasswordHasher.HashPassword(uobj.Password);
+            uobj.Token = "";
+            uobj.Role = "User";
             await _applicationDbContext.Users.AddAsync(uobj);
             await _applicationDbContext.SaveChangesAsync();
             return Ok(new
@@ -58,6 +96,32 @@ namespace Backend.Controllers
             });
            //return StatusCode(StatusCodes.Status200OK,"Registration Success");
 
+        }
+
+        private Task<bool> CheckUserNameExist(string username)
+           => _applicationDbContext.Users.AnyAsync(x => x.Username == username);
+
+
+        private Task<bool> CheckEmailExist(string email)
+           => _applicationDbContext.Users.AnyAsync(x => x.Email == email);
+
+
+        private string CheckPasswordStrength(string password)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (password.Length < 4)
+            {
+                sb.Append("Minimum password length should be 8" + Environment.NewLine);
+            }
+            if (!(Regex.IsMatch(password, "[a-z]") && Regex.IsMatch(password, "[A-Z]") && Regex.IsMatch(password, "[0-9]"))){
+                sb.Append("Password should be Alphanumric" + Environment.NewLine);
+            }
+           
+            if (!(Regex.IsMatch(password, "[<,>]")))
+            {
+                sb.Append("Password should contain special character" + Environment.NewLine);
+            }
+            return sb.ToString();
         }
     }
 }
