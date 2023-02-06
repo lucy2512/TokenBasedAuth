b603172df3ab612e7e86734355342ad576a0da9d
lucy2512 <mail4parthib@gmail.com>
2023-02-06 10:27:29 +0530

    Session Expired Error Handling Done

--- Frontend/src/app/interceptors/token.interceptor.ts
+++ Frontend/src/app/interceptors/token.interceptor.ts
@@ -3,15 +3,18 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 import { AuthService } from '../services/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService,private toastr: ToastrService,private router: Router) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const myToken = this.auth.getToken();
@@ -20,7 +23,17 @@ export class TokenInterceptor implements HttpInterceptor {
         setHeaders: {Authorization: `Bearer ${myToken}`}
       });
     }
-    return next.handle(request);
+    return next.handle(request).pipe(
+      catchError((err:any) => {
+        if(err instanceof HttpErrorResponse){
+        if(err.status === 401){
+          this.auth.signOut();
+          this.toastr.error("Your session has expired!", 'ERROR', { timeOut: 2000, positionClass: 'toast-top-right'});
+        }
+      }
+      return throwError(() => new Error("Some othe error occured!"))
+    })
+    );
   }
 }
 
